e812997aadc2e678b54e0b6911c76b22f125f35e
Marius TENE <tefuncowm@gmail.com>
2019-05-15 20:32:38 +0100

    test envoi de sms

--- src/main/java/agpe/AgpeApplication.java
+++ src/main/java/agpe/AgpeApplication.java
@@ -7,13 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import agpe.metier.AgpeMetier;
 import agpe.modeles.Role;
-import agpe.modeles.Utilisateur;
+import agpe.sms.Service;
+import agpe.sms.SmsRequest;
 
 @SpringBootApplication
 public class AgpeApplication implements CommandLineRunner{
 	
 	  @Autowired 
 	  private AgpeMetier agpeMetier;
+	  
+	  @Autowired
+	  private Service service;
 
 	 
 	public static void main(String[] args) {
@@ -35,6 +39,8 @@ public class AgpeApplication implements CommandLineRunner{
 		 * "15y511","groupe7","Yaounde","691103603","Yaounde",1,"Informatique",
 		 * "Professeur","Genie Logiciel",r); agpeMetier.enregistrerUTilisateur(user);
 		 */
+		SmsRequest smsRequest = new SmsRequest("+237699425427","welcome to agpe application.");
+		service.envoyerSms(smsRequest);
 		
 	}
 
