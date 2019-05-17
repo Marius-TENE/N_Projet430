7c644208f854813234f4dc8fd28f2245773a4d67
Marius TENE <tefuncowm@gmail.com>
2019-05-17 08:12:54 +0100

    ajout d'une interface de chat, mais sans adpter au projet

--- src/main/java/agpe/portfolio/controller/FileController.java
+++ src/main/java/agpe/portfolio/controller/FileController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,6 +33,11 @@ public class FileController {
     @Autowired
     private AgpeMetier agpeMetier;
 
+    
+    @RequestMapping(value  = "/addpiece")
+    public String addPiece() {
+    	return "piece";
+    }
     @PostMapping("/uploadFile")
     public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
     	Optional<Categorie> cat = agpeMetier.retournerCategorie(1);
